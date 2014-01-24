717ffd550f7b71b42fa2311b3f3c27dd0095f190
wyj2046 <wyj2046@gmail.com>
2014-01-24 10:32:55 +0800

    启动时自动加载tomorrow theme

--- config/wyj-ui.el
+++ config/wyj-ui.el
@@ -16,12 +16,17 @@
 ;; (require 'color-theme-blackboard)
 ;; (color-theme-blackboard)
 
-(require 'color-theme-molokai)
-(color-theme-molokai)
+;; (require 'color-theme-molokai)
+;; (color-theme-molokai)
 
 ;; (color-theme-initialize)  ;;对于内置theme，要加这句话
 ;; (color-theme-blue-mood)
 
+;; tomorrow theme from elpa
+;; 因为依赖于elpa, 所以.emacs配置中ui要在packages后加载
+(require 'color-theme-sanityinc-tomorrow)
+;; blue bright day eighties night
+(color-theme-sanityinc-tomorrow--define-theme eighties)
 
 ;; Linux下
 ;; 全屏
