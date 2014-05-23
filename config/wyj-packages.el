30d85dd080020b7d85f12b49c610c0565416aedf
wyj2046 <wyj2046@gmail.com>
2014-05-23 17:57:06 +0800

    add cflow plugin

--- config/wyj-packages.el
+++ config/wyj-packages.el
@@ -183,3 +183,31 @@ the mode-line."
                     'php-mode-hook
                     ))
   (add-hook hook 'hideshowvis-enable))
+
+
+;; cflow
+;; GNU cflow http://emacser.com/emacs-cflow.htm
+(add-to-list 'load-path "~/.emacs.d/plugins")
+(require 'cflow-mode)
+(defvar cmd nil nil)
+(defvar cflow-buf nil nil)
+(defvar cflow-buf-name nil nil)
+
+(defun cflow (function-name)
+  "Get call graph of inputed function. "
+					;(interactive "sFunction name:\n")
+  (interactive (list (car (senator-jump-interactive "Function name: "
+                                                    nil nil nil))))
+  (setq cmd (format "cflow  -b --main=%s %s" function-name buffer-file-name))
+  (setq cflow-buf-name (format "**cflow-%s:%s**"
+                               (file-name-nondirectory buffer-file-name)
+                               function-name))
+  (setq cflow-buf (get-buffer-create cflow-buf-name))
+  (set-buffer cflow-buf)
+  (setq buffer-read-only nil)
+  (erase-buffer)
+  (insert (shell-command-to-string cmd))
+  (pop-to-buffer cflow-buf)
+  (goto-char (point-min))
+  (cflow-mode)
+  )
